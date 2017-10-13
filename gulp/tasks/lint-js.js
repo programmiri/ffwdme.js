f9b605de7937e1e000539e672ca2258c1db6032a
programmiri <programmiri@gmail.com>
2017-10-13 18:08:36 +0200

    Add correct source to gulp file.

--- gulp/tasks/lint-js.js
+++ gulp/tasks/lint-js.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
-gulp.task('lint-js', function() {
-  return gulp.src('.build/**')
+gulp.task('lint-js', function lintJs() {
+  return gulp.src(['./src/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format());
 });
